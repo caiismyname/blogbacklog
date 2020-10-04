d1403b3f14c7c280b078fb8d7543a309a5f7ff06
caiismyname <davidcai2012@gmail.com>
2020-10-04 16:20:50 -0400

    Adding my initial backend code. Mail send / db update process works with dummy send

---
+++ src/backend/mail-daemon.js
@@ -0,0 +1,96 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('/Users/davidcai/Desktop/blogbacklog-cb6c3df2e9a2.json'); //TODO temp path
+const { DateTime } = require('luxon');
+
+// Firebase Initialization
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+  
+const db = admin.firestore();
+const feedsRef = db.collection('feeds');
+
+// Receive `now` to approximate a point-in-time run across multiple invocations
+function shouldSend(lastSentSeconds, frequency, dayOfWeek, now) {
+    lastSent = DateTime.fromSeconds(lastSentSeconds);
+    nextSend = lastSent.plus({days: frequency});
+
+    correctDate = now.hasSame(nextSend, 'day');
+    correctDayOfWeek = now.weekday === dayOfWeek; //  Luxon: 1 is Monday, 7 is Sunday
+
+    console.log(lastSent.toISODate(), nextSend.toISODate(), correctDate, correctDayOfWeek);
+
+    return (correctDate && correctDayOfWeek);
+}
+
+async function getFeedsToSend(now) {
+    const snapshot = await feedsRef.where('isActive', '==', true).get();
+
+    if (snapshot.empty) {
+        console.log('No matching documents.');
+        return;
+    };
+
+    snapshot.forEach(doc => {
+        console.log(doc.id, '=>', doc.data());
+    });
+
+    let toSend = [];
+    snapshot.forEach(feed => {
+        const schedule = feed.data().schedule;
+        if (shouldSend(schedule.lastSent._seconds, schedule.frequency, schedule.dayOfWeek, now)) {
+            toSend.push(feed);
+        }
+    });
+    return (toSend);
+}
+
+function sendMail(toSend) {
+    toSend.forEach(feed => {
+        console.log("Pretending to send" + feed.id);
+    });
+
+    return (toSend);
+}
+
+function createUpdatedDatabaseEntry(feed, now) {
+    var updatedFeed = { ... feed };
+    
+    if (feed.entries.length === 1) {
+        updatedFeed.isActive = false;
+    } else {
+        updatedFeed.lastSent = now.toISODate();
+        updatedFeed.entries = feed.entries.slice(1);
+    }
+
+    return (updatedFeed);
+}
+
+async function updateDatabaseWithSend(successfulSends, now) {
+    // Get a new write batch
+    // TODO: writes have a 500 item limit.
+    const batch = db.batch();
+    
+    successfulSends.forEach(feed => {
+        const feedRef = feedsRef.doc(feed.id);
+        batch.update(feedRef, createUpdatedDatabaseEntry(feed.data(), now));
+    });
+
+    // Commit the batch
+    await batch.commit();
+}
+
+async function processFeedsToSend(toSend, now) {
+    const successfulSends = await sendMail(toSend);
+    updateDatabaseWithSend(successfulSends, now);
+}
+
+async function start() {
+    let now = DateTime.local();
+
+    const toSend = await getFeedsToSend(now);
+    processFeedsToSend(toSend, now);
+}
+
+start();
---
+++ src/backend/server.js
@@ -0,0 +1,49 @@
+ const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const PORT = 4000;
+const admin = require('firebase-admin');
+const serviceAccount = require('//Users/davidcai/Desktop/blogbacklog-cb6c3df2e9a2.json'); //TODO temp path
+const router = express.Router();
+
+// App + Router initialization
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/", router);
+
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
+
+// Firebase Initialization
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+// Initialization Complete
+// Router below
+
+router.get('/feed', (req, res, next) => {
+
+});
+
+router.post('/newFeed', async (req, res, next) => {
+    const feedsRef = db.collection('feeds');
+
+    await feedsRef.add({
+      entries: ["foo", "bar"],
+      schedule: {
+        dayOfWeek: 3,
+        frequency: 7,
+      }
+    }).then(
+        res.json('Feed added')
+    );
+});
+
+module.exports = router;
