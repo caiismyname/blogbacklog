567d922e919901c994e22c6d72a776d091388e1b
David Cai <davidcai2012@gmail.com>
2020-11-29 10:09:17 -0700

    added (but notyet using) a basic email validation func

--- routes/process.js
+++ routes/process.js
@@ -42,6 +42,12 @@ function splitManualInputLinks(input) {
     return (emptiesRemoved);
 }
 
+// Very basic, just checking for something@something.something
+function isValidEmail(emailAddress) {
+    var re = "/\S+@\S+\.\S+/";
+    return (re.test(emailaddress));
+}
+
 // Routes
 
 // // Empty endpoint for now, intended to host a "view/edit" feature.
